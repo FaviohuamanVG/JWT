6c9f336a9c3baa9b63df1c9d45372c4a5dcc714a
Favio Huaman <141435482+FaviohuamanVG@users.noreply.github.com>
2026-05-05 23:12:15 +0000

    feat: Import User model in JwtService for enhanced token handling

--- src/main/java/despliegue/exercises/config/JwtService.java
+++ src/main/java/despliegue/exercises/config/JwtService.java
@@ -1,5 +1,6 @@
 package despliegue.exercises.config;
 
+import despliegue.exercises.models.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
